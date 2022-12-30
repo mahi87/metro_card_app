d37c6270f8298b2576ea22e0bc90987e1738b5c7
Mahima choudhary <mahi87mnit@gmail.com>
2022-12-31 00:33:25 +0530

    add station model

--- app/models.py
+++ app/models.py
@@ -13,3 +13,13 @@ class MetroCard(db.Model):
 
     def check_pin(self, pin):
         return check_password_hash(self.pin_hash, pin)
+
+
+class Station(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    total_collection = db.Column(db.Integer, default=0)
+    total_discount = db.Column(db.Integer, default=0)
+    passenger_adult = db.Column(db.Integer, default=0)
+    passenger_kid = db.Column(db.Integer, default=0)
+    passenger_senior_citizen = db.Column(db.Integer, default=0)
